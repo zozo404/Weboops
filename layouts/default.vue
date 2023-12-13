260d77f9a38fe2d52dfe498c63fff90331699967
zozo404 <enzocuny66@gmail.com>
2023-12-13 19:04:12 +0100

    🎉 | start weboops

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="selection:bg-cyan-500 selection:text-black">
+    <Header />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
+<style>
+html{
+  font-family: 'Comfortaa';
+}
+</style>
